a5164373044aaeef18f42fe619880ea3f7c916e4
Assim Al-Marhuby <assim1991@gmail.com>
2013-07-26 20:51:40 +0400

    Added an auto-update of departments_count and items_count rows of the data table.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -2,6 +2,9 @@
 class Main extends CI_Controller {
 
 	public function index() {
+		$this->db->query("UPDATE data SET value = (SELECT COUNT(*) FROM departments) WHERE name = 'departments_count'");
+		$this->db->query("UPDATE data SET value = (SELECT COUNT(*) FROM items) WHERE name = 'items_count'");
+
 		$query = $this->db->query("SELECT * FROM data");
 		echo json_encode($query->result());
 	}
