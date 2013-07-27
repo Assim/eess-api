5eb1efdebef9d6dadd8e83f8ae204e5e6b6ed86c
Assim Al-Marhuby <assim1991@gmail.com>
2013-07-27 21:54:43 +0400

    Added item images to the API.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,6 +5,18 @@ class Main extends CI_Controller {
 		$this->db->query("UPDATE data SET value = (SELECT COUNT(*) FROM departments) WHERE name = 'departments_count'");
 		$this->db->query("UPDATE data SET value = (SELECT COUNT(*) FROM items) WHERE name = 'items_count'");
 
+		$this->db->query("UPDATE items SET picture = null");
+		$dir = "images/items/";
+		if(is_dir($dir)) {
+			if ($dh = opendir($dir)) {
+				while (($file = readdir($dh)) !== false) {
+					$item_id = substr($file, 0, strpos($file, '.'));
+					$this->db->query("UPDATE items SET picture = '".$file."' WHERE item_id = '".$item_id."'");
+				}
+				closedir($dh);
+			}
+		}
+
 		$query = $this->db->query("SELECT * FROM data");
 		echo json_encode($query->result());
 	}
